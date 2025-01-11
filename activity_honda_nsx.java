e21b80eb8a166626f0dff735d35f4a1886258aa1
rifkyalwansyah <147989803+rifkyalwansyah@users.noreply.github.com>
2025-01-11 12:49:47 +0700

    Create activity_honda_nsx.java

---
+++ activity_honda_nsx.java
@@ -0,0 +1,14 @@
+package com.as.jdmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_honda_nsx extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_honda_nsx);
+    }
+}
