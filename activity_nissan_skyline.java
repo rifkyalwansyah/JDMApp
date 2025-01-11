6347b7f5ec244262924c1cb0c1e57308911dddfe
rifkyalwansyah <147989803+rifkyalwansyah@users.noreply.github.com>
2025-01-11 12:51:27 +0700

    Create activity_nissan_skyline.java

---
+++ activity_nissan_skyline.java
@@ -0,0 +1,14 @@
+package com.as.jdmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_nissan_skyline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nissan_skyline);
+    }
+}
