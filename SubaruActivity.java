58c83fe7517935e5853925c79ab6f01619646ada
rifkyalwansyah <147989803+rifkyalwansyah@users.noreply.github.com>
2025-01-11 12:57:04 +0700

    Update SubaruActivity.java

--- SubaruActivity.java
+++ SubaruActivity.java
@@ -2,7 +2,10 @@ package com.as.jdmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SubaruActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SubaruActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subaru);
+        // Temukan tombol Category
+        Button button = findViewById(R.id.buttonwrx);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent untuk pindah ke CategoryActivity
+                Intent intent = new Intent(SubaruActivity.this, activity_subaru_wrx.class);
+                startActivity(intent);
+            }
+        });
     }
 }
