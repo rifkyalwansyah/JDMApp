569b62fb6e56d882a0f1dbc15bf65c52815b331c
rifkyalwansyah <147989803+rifkyalwansyah@users.noreply.github.com>
2025-01-11 12:55:39 +0700

    Update MazdaActivity.java

--- MazdaActivity.java
+++ MazdaActivity.java
@@ -1,8 +1,10 @@
 package com.as.jdmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MazdaActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MazdaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mazda);
+
+        // Temukan tombol Category
+        Button button = findViewById(R.id.button_mazda);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent untuk pindah ke CategoryActivity
+                Intent intent = new Intent(MazdaActivity.this, activity_mazda_rx7.class);
+                startActivity(intent);
+            }
+        });
     }
 }
