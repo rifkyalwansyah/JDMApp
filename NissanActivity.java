460cb4437f80aee1863af33f58bdd871c731f32c
rifkyalwansyah <147989803+rifkyalwansyah@users.noreply.github.com>
2025-01-11 12:56:46 +0700

    Update NissanActivity.java

--- NissanActivity.java
+++ NissanActivity.java
@@ -2,7 +2,10 @@ package com.as.jdmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NissanActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class NissanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nissan);
+        // Temukan tombol Category
+        Button button = findViewById(R.id.button_skyline);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent untuk pindah ke CategoryActivity
+                Intent intent = new Intent(NissanActivity.this, activity_nissan_skyline.class);
+                startActivity(intent);
+            }
+        });
     }
 }
