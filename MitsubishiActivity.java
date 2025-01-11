bfe0449c5e3fe4a71c66de11b63596be47db57b6
rifkyalwansyah <147989803+rifkyalwansyah@users.noreply.github.com>
2025-01-11 12:55:59 +0700

    Update MitsubishiActivity.java

--- MitsubishiActivity.java
+++ MitsubishiActivity.java
@@ -2,7 +2,10 @@ package com.as.jdmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MitsubishiActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MitsubishiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mitsubishi);
+        // Temukan tombol Category
+        Button button = findViewById(R.id.buttonevo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent untuk pindah ke CategoryActivity
+                Intent intent = new Intent(MitsubishiActivity.this, activity_mitshubishi_lancer_evo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
