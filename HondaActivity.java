a4cce954414214581de09b901798dcd9395a7c7f
rifkyalwansyah <147989803+rifkyalwansyah@users.noreply.github.com>
2025-01-11 12:55:15 +0700

    Update HondaActivity.java

--- HondaActivity.java
+++ HondaActivity.java
@@ -1,8 +1,10 @@
 package com.as.jdmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HondaActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class HondaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_honda);
+
+        // Temukan tombol Category
+        Button button = findViewById(R.id.button_nsx);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent untuk pindah ke CategoryActivity
+                Intent intent = new Intent(HondaActivity.this, activity_honda_nsx.class);
+                startActivity(intent);
+            }
+        });
     }
 }
